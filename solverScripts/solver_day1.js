950dfb2eb24bbe18470080f9b39a9ddb91529dd9
Zac Ingoglia <zacace@ingoglia.com>
2020-02-06 22:26:10 -0700

    Minor fixes
    Updated the button names for the HTML file
    Wrote test code to day 1 puzzle a/1

--- solverScripts/solver_day1.js
+++ solverScripts/solver_day1.js
@@ -1,1 +1,12 @@
 "use strict";
+
+function day1a(){
+    getInputData();
+    console.log("test");
+}
+
+function getInputData(){
+    var puzzleInput = document.getElementById("input").value;
+    var answerOutput = document.getElementById("output");
+    console.log(puzzleInput);
+}
